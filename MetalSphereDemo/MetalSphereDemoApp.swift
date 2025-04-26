4163b59c9c89ee9e85a149597c967cd53b7e3afe
Dale Carman <dale.carman@gmail.com>
2025-04-26 05:50:01 -0500

    Initial Commit

---
+++ MetalSphereDemo/MetalSphereDemoApp.swift
@@ -0,0 +1,34 @@
+//
+//  MetalSphereDemoApp.swift
+//  MetalSphereDemo
+//
+//  Created by Dale Carman on 4/26/25.
+//
+
+import SwiftUI
+
+@main
+struct MetalSphereDemoApp: App {
+
+    @State private var appModel = AppModel()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(appModel)
+        }
+        .windowStyle(.volumetric)
+
+        ImmersiveSpace(id: appModel.immersiveSpaceID) {
+            ImmersiveView()
+                .environment(appModel)
+                .onAppear {
+                    appModel.immersiveSpaceState = .open
+                }
+                .onDisappear {
+                    appModel.immersiveSpaceState = .closed
+                }
+        }
+        .immersionStyle(selection: .constant(.mixed), in: .mixed)
+    }
+}
---
+++ MetalSphereDemoTests/MetalSphereDemoTests.swift
@@ -0,0 +1,17 @@
+//
+//  MetalSphereDemoTests.swift
+//  MetalSphereDemoTests
+//
+//  Created by Dale Carman on 4/26/25.
+//
+
+import Testing
+@testable import MetalSphereDemo
+
+struct MetalSphereDemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
